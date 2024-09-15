85d3d0630ce4db1801b680121282d2bcae03ac2b
Destin Ashad <destin.yana@resultstack.com>
2024-09-14 20:41:34 -0400

    refactor hooks

---
+++ src/models/Coin.ts
@@ -0,0 +1,51 @@
+
+export interface Coin {
+    coinInfo: {
+        fullName: string,
+        shortName: string,
+        imageUrl?: string,
+        url?: string
+    },
+    raw: {
+        fromSymbol?: string;
+        toSymbol?: string;
+        price?: number;
+        openHour?: number;
+        highHour: number;
+        lowHour: number;
+        openDay: number;
+        highDay: number;
+        lowDay: number;
+        open24Hour: number;
+        high24Hour: number;
+        low24Hour: number;
+        change24Hour: number;
+        changePct24Hour: number;
+        changeDay: number;
+        changePctDay: number;
+        changeHour: number;
+        changePctHour: number;
+        imageUrl: string;
+    },
+    display: {
+        fromSymbol: string;
+        toSymbol: string;
+        price: string;
+        openHour: string;
+        highHour: string;
+        lowHour: string;
+        openDay: string;
+        highDay: string;
+        lowDay: string;
+        open24Hour: string;
+        high24Hour: string;
+        low24Hour: string;
+        change24Hour: string;
+        changePct24Hour: string;
+        changeDay: string;
+        changePctDay: string;
+        changeHour: string;
+        changePctHour: string;
+        imageUrl: string;
+    }
+}
